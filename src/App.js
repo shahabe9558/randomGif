4ba386e53a89f96a8a637f3c1a735630d3539ed7
Shahabenawaz Alam <shahabe9559@gmail.com>
2023-12-11 18:51:04 +0530

    react and tailwind setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+
+  return (
+    <div className="w-[100%] h-[100%] bg-richblack-900 flex flex-col">
+       This a React App
+    </div>
+  );
+}
+
+export default App;
